9f496af7b034a7f8c24b7ab49f7ebd99ac3a5f6e
Sni <jeremy.snidaro@gmail.com>
2021-06-02 19:58:29 +0200

    Fix error on sed

--- install.sh
+++ install.sh
@@ -16,6 +16,6 @@ git clone https://gitlab.com/andreacarrieroo/devops-technical-test /opt/devops-t
 
 # Replace listening Ip and Allowed host to enable access to the website
 sed -i 's/127.0.0.1/0.0.0.0/g' /opt/devops-technical-test/Dockerfile
-sed -i 's/*/example.com/g' /opt/devops-technical-test/django_project/django_project/settings.py
+sed -i 's/example.com/*/g' /opt/devops-technical-test/django_project/django_project/settings.py
 
 cd /opt/devops-technical-test && docker-compose up -d
